9b7ab5fd0013d5806012b194b1e6b9ea78982ca0
Younghoo Kim <sensitive.hu@gmail.com>
2023-09-02 21:01:00 +0900

    refactor: GlobalCss 컴포넌트 (#94)

--- src/components/GlobalCss.tsx
+++ src/components/GlobalCss.tsx
@@ -3,77 +3,89 @@ import Font from '../fonts/Font';
 
 const GlobalCss = () => {
   return (
-    <Global
-      styles={css`
-        ${Font}
-
-        :root {
-          --colors-primary: #d58135;
-          --colors-secondary: #cf9a12;
-        }
-
-        :root .dark {
-          --dark-background: #111314;
-          --dark-font: #fff;
-        }
-
-        :root .light {
-          --dark-background: #fff;
-          --dark-font: #111314;
-        }
-
-        *,
-        *::before,
-        *::after {
-          box-sizing: border-box;
-        }
-
-        html,
-        body {
-          height: 100%;
-          font-family: 'Noto Sans KR', sans-serif;
-        }
-
-        body {
-          margin: 0;
-          line-height: 1.5;
-          -webkit-font-smoothing: antialiased;
-          background: var(--dark-background);
-          color: var(--dark-font);
-        }
-
-        img,
-        picture,
-        video,
-        canvas,
-        svg {
-          display: block;
-          max-width: 100%;
-        }
-
-        input,
-        button,
-        textarea,
-        select {
-          font: inherit;
-        }
-
-        p,
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          overflow-wrap: break-word;
-        }
-
-        #___gatsby {
-          isolation: isolate;
-        }
-      `}
-    />
+    <>
+      <Global
+        styles={css`
+          ${globalCss}
+          ${resetCss}
+        `}
+      />
+    </>
   );
 };
 
 export default GlobalCss;
+
+const globalCss = css`
+  ${Font}
+
+  :root {
+    --colors-primary: #d58135;
+    --colors-secondary: #cf9a12;
+  }
+
+  :root .dark {
+    --dark-background: #111314;
+    --dark-font: #fff;
+  }
+
+  :root .light {
+    --dark-background: #fff;
+    --dark-font: #111314;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
+
+const resetCss = css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+    background: var(--dark-background);
+    color: var(--dark-font);
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+  }
+
+  #___gatsby {
+    isolation: isolate;
+  }
+`;

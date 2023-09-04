c9839df38a7840203532d6981a283adc986e3d88
Younghoo Kim <sensitive.hu@gmail.com>
2023-09-04 23:30:51 +0900

    refactor: Typograhpy 컴포넌트

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,31 +1,22 @@
 import { StrictPropsWithChildren } from '@customTypes/common';
+import { Theme } from '@emotion/react';
 import styled from '@emotion/styled';
-import { CSSProperties, ElementType } from 'react';
+import { ElementType } from 'react';
 
-type Variant =
-  | 'h1'
-  | 'h2'
-  | 'h3'
-  | 'logo'
-  | 'label'
-  | 'subtitle'
-  | 'body'
-  | 'div';
+type Variant = 'h1' | 'h2' | 'h3' | 'label' | 'body';
 
 interface TypographyProps {
   variant?: Variant;
-  as: ElementType;
-  style?: CSSProperties;
+  as?: ElementType;
 }
 
 const Typography = ({
   children,
   as,
-  style,
-  variant = 'div',
+  variant = 'body',
 }: StrictPropsWithChildren<TypographyProps>) => {
   return (
-    <Wrapper as={as} variant={variant} style={style}>
+    <Wrapper as={as} variant={variant}>
       {children}
     </Wrapper>
   );
@@ -33,25 +24,9 @@ const Typography = ({
 
 export default Typography;
 
-const Wrapper = styled('div')<{ variant: Variant }>(({ variant }) => ({
-  ...getStyle(variant),
+const Wrapper = styled('div')<{ variant: Variant }>(({ variant, theme }) => ({
+  ...getStyle(variant, theme),
 }));
 
-const getStyle = (variant: Variant) => {
-  switch (variant) {
-    case 'h1':
-      return { fontSize: '30px', fontWeight: 700 };
-    case 'h2':
-      return { fontSize: '22px', fontWeight: 600 };
-    case 'h3':
-      return { fontSize: '18px', fontWeight: 500 };
-    case 'logo':
-      return { fontSize: '30px', fontWeight: 700 };
-    case 'label':
-      return { fontSize: '14px', fontWeight: 500 };
-    case 'body':
-      return { fontSize: '16px', fontWeight: 500 };
-    default:
-      return {};
-  }
-};
+const getStyle = (variant: Variant, theme: Theme) =>
+  theme.typography[variant] || {};

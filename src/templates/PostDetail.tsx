f41a3844a86b70c24eacbebf0566318f1dfdb367
wherehows <sensitive.hu@gmail.com>
2023-09-15 22:05:44 +0900

    refactor: 상세 페이지 데이터를 쿼리할 때 가져오도록 수정

--- src/templates/PostDetail.tsx
+++ src/templates/PostDetail.tsx
@@ -1,7 +1,7 @@
 import { CollapsibleSidebar, FixedSidebar } from '@/components/Sidebar';
 import GlobalCss from '@/components/GlobalCss';
 import { graphql, PageProps } from 'gatsby';
-import { Edge } from '@/types/document';
+import { Edge, Node } from '@/types/document';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/utils/const';
 import useResponsiveWeb from '@/hooks/useResponsiveWeb';
@@ -14,26 +14,28 @@ import {
 } from '@/components/PostDetailContent';
 
 interface QueryResultType {
-  allMarkdownRemark: { edges: Edge[] };
+  allPosts: { edges: Edge[] };
+  selectedPost: Node;
+}
+
+interface PageContextType {
+  slug: string;
 }
 
 export default function PostDetail({
   data: {
-    allMarkdownRemark: { edges },
+    allPosts: { edges },
+    selectedPost,
   },
-  path,
-}: PageProps<QueryResultType>) {
+  pageContext: { slug },
+}: PageProps<QueryResultType, PageContextType>) {
   const [isSidebarShown, setIsSidebarShown] = useState(false);
-  const folderInformations = getFolders(edges);
-  const res = getSelectedDocument(edges, path);
   const { isUnder960px } = useResponsiveWeb();
+  const path = slug;
 
-  // TODO: 게시글이 존재하지 않는 경우에 대한 에러 핸들링 필요
-  if (!res) {
-    return <></>;
-  }
-
-  const { html: selectedDocument, title } = res;
+  const folderInformations = getFolders(edges);
+  const selectedDocument = selectedPost.html;
+  const { title } = selectedPost.frontmatter;
 
   return (
     <>
@@ -71,27 +73,22 @@ export default function PostDetail({
   );
 }
 
-const getSelectedDocument = (edges: Edge[], targetDocumentPath: string) => {
-  const edge = edges.find(({ node }: Edge) => {
-    if (node.frontmatter.slug === targetDocumentPath) {
-      return true;
-    }
-  });
-
-  let res = null;
-
-  if (edge) {
-    const { frontmatter, html } = edge.node;
-    res = { title: frontmatter.title, html };
-  }
-
-  return res;
-};
-
 export const getPosts = graphql`
-  query {
-    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+  query ($slug: String!) {
+    allPosts: allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
       ...MarkdownRemarkFields
+    }
+    selectedPost: markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        date
+        folder
+        slug
+        subTitle
+        title
+      }
+      html
     }
   }
 `;

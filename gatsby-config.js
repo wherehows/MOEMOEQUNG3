dcde6c3e660c2fd15945b85e17d738fa11cfefa8
Younghoo Kim <sensitive.hu@gmail.com>
2023-10-31 21:39:09 +0900

    chore: typegen 기능 on

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   flags: {
     DEV_SSR: true,
   },
+  graphqlTypegen: true,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,

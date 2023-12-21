129301669aa05d8a5d9ffcac092dc8053b401deb
wherehows <sensitive.hu@gmail.com>
2023-12-21 17:42:54 +0900

    feat(List): 년월일 제공되는 형태 변경

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -6,5 +6,5 @@ export const changeUTCToYYYYMMDD = (utc: string) => {
   const month = (kstDate.getMonth() + 1).toString().padStart(2, '0');
   const day = kstDate.getDate().toString().padStart(2, '0');
 
-  return `${year}/${month}/${day}`;
+  return `${year}-${month}-${day}`;
 };

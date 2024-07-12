099cf83973c01e558c74a9e8c3362a2bc657932c
wherehows <sensitive.hu@gmail.com>
2024-07-12 19:44:47 +0900

    chore: make design for block quote

--- src/components/PostDetailContent.tsx
+++ src/components/PostDetailContent.tsx
@@ -25,6 +25,93 @@ export const PostDetailContent = ({ selectedPost }: ContentProps) => {
     <Wrapper>
       <Typography variant="h1">{title}</Typography>
       <ViewCounter slug={slug.current} />
+      <blockquote
+        style={{
+          fontSize: '18px',
+          color: '#444',
+          borderLeft: '8px solid #2c2c2c',
+          padding: '8px 16px',
+          margin: '18px 0',
+          background: '#ffffff',
+          boxShadow: '0 1px 4px rgba(0, 0, 0, 0.1)',
+          fontStyle: 'normal',
+        }}
+      >
+        <div>
+          아는 것과 모르는 것은 다르고, 제대로 아는 것과 아는 것은 또 다르다.
+          <br />
+          아는 것은 아는 것 대로 중요하고, 제대로 아는 것은 제대로 아는 것 대로
+          중요하다.
+        </div>
+        <cite
+          style={{
+            display: 'block',
+            textAlign: 'right',
+            marginTop: '20px',
+            color: '#2c2c2c',
+            fontWeight: 'bold',
+          }}
+        >
+          블로그 주인장의 첫 회사 사수
+        </cite>
+      </blockquote>
+      {/* <blockquote
+        style={{
+          fontSize: '1.2em',
+          color: '#333',
+          borderLeft: '15px solid #ccc',
+          padding: '8px 12px',
+          margin: '18px 0',
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+          // background: '#f9f9f9',
+        }}
+      >
+        <div>
+          아는 것과 모르는 것은 다르고, 제대로 아는 것과 아는 것은 또 다르다.
+          <br />
+          아는 것은 아는 것 대로 중요하고, 제대로 아는 것은 제대로 아는 것 대로
+          중요하다
+        </div>
+        <cite
+          style={{
+            display: 'block',
+            textAlign: 'right',
+            marginTop: '22px',
+          }}
+        >
+          - 블로그 주인장의 첫 회사 사수
+        </cite>
+      </blockquote> */}
+      {/* <blockquote
+        style={
+          {
+            // margin: '12px 0',
+            // position: 'relative',
+            // fontSize: '16px',
+            // padding: '8px 12px  ',
+            // borderLeft: '15px solid var(--colors-violet-02)',
+            // '-moz-box-shadow': '2px 2px 15px #ccc',
+            // '-webkit-box-shadow': '2px 2px 15px #ccc',
+            // boxShadow: '2px 2px 15px #ccc',
+          }
+        }
+      >
+        <div>
+          아는 것과 모르는 것은 다르고, 제대로 아는 것과 아는 것은 또 다르다.
+          <br />
+          아는 것은 아는 것 대로 중요하고, 제대로 아는 것은 제대로 아는 것 대로
+          중요하다
+        </div>
+        <cite
+          style={{
+            display: 'block',
+            textAlign: 'right',
+            marginTop: '22px',
+          }}
+        >
+          - 블로그 주인장의 첫 회사 사수
+        </cite>
+      </blockquote> */}
       <Markdown
         children={content}
         components={{
